dd7f8186eaa7bc67c967ebbe1a14d8d78284dfee
andrew emets <andrew.emets@gmail.com>
2019-04-14 23:50:35 +0300

    Render ray directions in front of the camera

---
+++ Assets/Raymarch.shader
@@ -0,0 +1,63 @@
+﻿Shader "Custom/Raymarch"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            uniform float4x4 _CamFrustum, _CamToWorld;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 ray : TEXCOORD1;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                half index = v.vertex.z;
+                v.vertex.z = 0;
+                
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;                
+
+                o.ray = _CamFrustum[(int)index].xyz;
+                o.ray /= abs(o.ray.z);
+                o.ray = mul(_CamToWorld, o.ray);
+                
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float3 rayDir = normalize(i.ray.xyz);
+                float3 rayOrigin = _WorldSpaceCameraPos;
+                return fixed4(rayDir, 1);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/RaymarchCamera.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RaymarchCamera : MonoBehaviour
+{
+    [SerializeField] private Shader shader;
+
+    public Material Material
+    {
+        get
+        {
+            if (material == null)
+            {
+                if (shader != null)
+                    material = new Material(shader) {hideFlags = HideFlags.HideAndDontSave};
+            }
+
+            return material;
+        }
+    }
+
+    private Material material;
+
+    private Camera camera;
+    public Camera Camera
+    {
+        get
+        {
+            if (camera == null)
+            {
+                camera = GetComponent<Camera>();
+            }
+
+            return camera;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (Material == null)
+        {
+            Graphics.Blit(src, dest);
+        }
+
+        material.SetMatrix("_CamFrustum", CamFrustum(Camera));
+        material.SetMatrix("_CamToWorld", Camera.cameraToWorldMatrix);
+        
+        RenderTexture.active = dest;
+        
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        material.SetPass(0);
+
+        GL.Begin(GL.QUADS);
+        
+        // BL
+        GL.MultiTexCoord2(0, 0f, 0f);
+        GL.Vertex3(0f, 0f, 3f);
+
+        // TL
+        GL.MultiTexCoord2(0, 0f, 1f);
+        GL.Vertex3(0f, 1f, 0f);
+
+        // TR
+        GL.MultiTexCoord2(0, 1f, 1f);
+        GL.Vertex3(1f, 1f, 1f);
+        
+        // BR
+        GL.MultiTexCoord2(0, 1f, 0f);
+        GL.Vertex3(1f, 0f, 2f);
+        
+        
+        GL.End();
+        
+        GL.PopMatrix();
+    }
+
+    Matrix4x4 CamFrustum(Camera cam)
+    {
+        var frust = Matrix4x4.identity;
+        var fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        var up = Vector3.up * fov;
+        var right = Vector3.right * fov * Camera.aspect;
+
+        var TL = -Vector3.forward - right + up;
+        var TR = -Vector3.forward + right + up;
+        var BR = -Vector3.forward + right - up;
+        var BL = -Vector3.forward - right - up;
+        
+        frust.SetRow(0, TL);
+        frust.SetRow(1, TR);
+        frust.SetRow(2, BR);
+        frust.SetRow(3, BL);
+        
+        return frust;
+    }
+}
